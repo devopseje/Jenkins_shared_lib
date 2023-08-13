71e47a431128beac13eac4085a83b24f7f0e6253
Guillaume Ejangue <g.ejangue@aftech-solution.com>
2023-08-13 13:15:02 +0200

    first commit

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,8 @@
+def call(Map stageParams){
+
+    Checkout([
+        $class: 'GitSCM',
+        branches: [[name: stageParams.branch ]],
+        userRemoteConfigs: [[url: stageParams.url]]
+    ])
+}
